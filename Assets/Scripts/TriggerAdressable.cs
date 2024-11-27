2489a9c308cda19d06cb8af6249df288102c03f5
Rafael Okabe <rafaelokabe@gmail.com>
2024-11-27 18:42:12 -0300

    34

--- Assets/Scripts/TriggerAdressable.cs
+++ Assets/Scripts/TriggerAdressable.cs
@@ -7,8 +7,8 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class TriggerAdressable : MonoBehaviour
 {
-    //public List<AssetReference> assetReferences =new List<AssetReference>();
-    public List<PrefabAseetsGroup> assetReferences = new List<PrefabAseetsGroup>();
+    public List<AssetReference> assetReferences =new List<AssetReference>();
+    //public List<PrefabAseetsGroup> assetReferences = new List<PrefabAseetsGroup>();
 
 
     List<GameObject> prefabs = new List<GameObject>();
@@ -41,19 +41,19 @@ public class TriggerAdressable : MonoBehaviour
     {
         AsyncOperationHandle<GameObject> handle = new AsyncOperationHandle<GameObject>();
 
-        foreach (PrefabAseetsGroup asset in assetReferences)
-        {
-            prefabs = asset.InstantiatAssets(handle);
-        }
-
-        //foreach (AssetReference asset in assetReferences)
+        //foreach (PrefabAseetsGroup asset in assetReferences)
         //{
-        //    handle = asset.InstantiateAsync(transform.position, Quaternion.identity);
-        //    handle.Completed += handle =>
-        //    {
-        //        prefabs.Add(handle.Result);
-        //    };
+        //    prefabs = asset.InstantiatAssets(handle);
         //}
+
+        foreach (AssetReference asset in assetReferences)
+        {
+            handle = asset.InstantiateAsync(transform.position, Quaternion.identity);
+            handle.Completed += handle =>
+            {
+                prefabs.Add(handle.Result);
+            };
+        }
 
         //Code for just one prefab
         //handle = assetReferences.InstantiateAsync(transform.position, Quaternion.identity);
